a8ff201e5d6e0b1e9a5f8eca2957470052b6e410
jtellis <jessetellis@gmail.com>
2020-09-21 15:11:28 -0400

    Create card deck

---
+++ src/Cards.hs
@@ -0,0 +1,22 @@
+module Cards (
+    deck
+) where
+
+data Rank = Ace | Two | Three | Four | Five
+          | Six | Seven | Eight | Nine | Ten
+          | Jack | Queen | King
+        deriving (Show, Enum)
+
+data Suit = Clubs | Diamonds | Hearts | Spades
+        deriving (Show, Enum)
+
+data Card = Card { suit :: Suit
+                 , rank :: Rank }
+
+instance Show Card where
+    show (Card s r) = show r ++ " of " ++ show s
+
+type Deck = [Card]
+
+deck::Deck
+deck = Card <$> [Clubs .. Spades] <*> [Ace .. King]
